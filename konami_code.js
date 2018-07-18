8c3c2c282be1cfc71b493c46041270c79dac652c
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-18 18:15:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,24 +11,24 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // `keySequence` will keep track of the last n keystrokes, where n === `codes.length`. `keySequence` will be compared to `codes` for each keydown event.
-  var keySequence = []
+// function init() {
+//   // `keySequence` will keep track of the last n keystrokes, where n === `codes.length`. `keySequence` will be compared to `codes` for each keydown event.
+//   var keySequence = []
   
-  var body = document.querySelector('body')
+//   var body = document.querySelector('body')
   
-  body.addEventListener('keydown', function(e) {
-    if (keySequence.length >= codes.length) {
-      keySequence.shift(e.key)
-    }
-    keySequence.push(e.key)
+//   body.addEventListener('keydown', function(e) {
+//     if (keySequence.length >= codes.length) {
+//       keySequence.shift(e.key)
+//     }
+//     keySequence.push(e.key)
     
-    // console.log(keySequence)
+//     // console.log(keySequence)
     
-    if (JSON.stringify(keySequence) === JSON.stringify(codes)) {
-      alert("Konami code activated!")
-    }
-  })
-}
+//     if (JSON.stringify(keySequence) === JSON.stringify(codes)) {
+//       alert("Konami code activated!")
+//     }
+//   })
+// }
 
 init()
